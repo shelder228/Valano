862d2c6437953b5b69142f8cea4d73997256e1ae
leshabl <shelder228@gmail.com>
2025-06-26 16:32:36 +0300

    21rasg
    123ga

---
+++ Il2CppOutputProject/Source/il2cppOutput/Unity.Advertisement.IosSupport.cpp
@@ -0,0 +1,176 @@
+﻿#include "pch-cpp.hpp"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include <limits>
+
+
+
+struct CharU5BU5D_t799905CF001DD5F13F7DBB310181FC4D8B7D0AAB;
+
+IL2CPP_EXTERN_C RuntimeClass* Application_tDB03BE91CDF0ACA614A5E0B67CFB77C44EB19B21_il2cpp_TypeInfo_var;
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+struct U3CModuleU3E_tB510D8ECAF4961EAF4BC2B629E78192052EABFD9 
+{
+};
+struct ATTrackingStatusBinding_t0114B0FAE0EF07B5CF980C573DD5402F9A6BDA23  : public RuntimeObject
+{
+};
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F  : public RuntimeObject
+{
+};
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F_marshaled_pinvoke
+{
+};
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F_marshaled_com
+{
+};
+struct Enum_t2A1A94B24E3B776EEF4E5E485E290BB9D4D072E2  : public ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F
+{
+};
+struct Enum_t2A1A94B24E3B776EEF4E5E485E290BB9D4D072E2_marshaled_pinvoke
+{
+};
+struct Enum_t2A1A94B24E3B776EEF4E5E485E290BB9D4D072E2_marshaled_com
+{
+};
+struct Int32_t680FF22E76F6EFAD4375103CBBFFA0421349384C 
+{
+	int32_t ___m_value;
+};
+struct Void_t4861ACF8F4594C3437BB48B6E56783494B843915 
+{
+	union
+	{
+		struct
+		{
+		};
+		uint8_t Void_t4861ACF8F4594C3437BB48B6E56783494B843915__padding[1];
+	};
+};
+struct RuntimePlatform_t9A8AAF204603076FCAAECCCC05DA386AEE7BF66E 
+{
+	int32_t ___value__;
+};
+struct AuthorizationTrackingStatus_tE8E49336CFC68D4B5DD2190E1D9474C7BEAC2BB3 
+{
+	int32_t ___value__;
+};
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+
+
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR int32_t Application_get_platform_m59EF7D6155D18891B24767F83F388160B1FF2138 (const RuntimeMethod* method) ;
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR void ATTrackingStatusBinding_InterfaceTrackingAuthorizationRequest_m44D02FD388917E34E0DD02072A71748081C02C65 (const RuntimeMethod* method) ;
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR int32_t ATTrackingStatusBinding_InterfaceGetTrackingAuthorizationStatus_m5BA14B6630FE5730137A92B51E99BEE1AA95DFD7 (const RuntimeMethod* method) ;
+IL2CPP_EXTERN_C void DEFAULT_CALL InterfaceTrackingAuthorizationRequest();
+IL2CPP_EXTERN_C int32_t DEFAULT_CALL InterfaceGetTrackingAuthorizationStatus();
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR void ATTrackingStatusBinding_InterfaceTrackingAuthorizationRequest_m44D02FD388917E34E0DD02072A71748081C02C65 (const RuntimeMethod* method) 
+{
+	typedef void (DEFAULT_CALL *PInvokeFunc) ();
+
+	reinterpret_cast<PInvokeFunc>(InterfaceTrackingAuthorizationRequest)();
+
+}
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR int32_t ATTrackingStatusBinding_InterfaceGetTrackingAuthorizationStatus_m5BA14B6630FE5730137A92B51E99BEE1AA95DFD7 (const RuntimeMethod* method) 
+{
+	typedef int32_t (DEFAULT_CALL *PInvokeFunc) ();
+
+	int32_t returnValue = reinterpret_cast<PInvokeFunc>(InterfaceGetTrackingAuthorizationStatus)();
+
+	return returnValue;
+}
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR void ATTrackingStatusBinding_RequestAuthorizationTracking_mCBF4621552E7D027AFB9161BA356A0D2E8CC311D (const RuntimeMethod* method) 
+{
+	static bool s_Il2CppMethodInitialized;
+	if (!s_Il2CppMethodInitialized)
+	{
+		il2cpp_codegen_initialize_runtime_metadata((uintptr_t*)&Application_tDB03BE91CDF0ACA614A5E0B67CFB77C44EB19B21_il2cpp_TypeInfo_var);
+		s_Il2CppMethodInitialized = true;
+	}
+	{
+		il2cpp_codegen_runtime_class_init_inline(Application_tDB03BE91CDF0ACA614A5E0B67CFB77C44EB19B21_il2cpp_TypeInfo_var);
+		int32_t L_0;
+		L_0 = Application_get_platform_m59EF7D6155D18891B24767F83F388160B1FF2138(NULL);
+		if ((!(((uint32_t)L_0) == ((uint32_t)8))))
+		{
+			goto IL_000d;
+		}
+	}
+	{
+		ATTrackingStatusBinding_InterfaceTrackingAuthorizationRequest_m44D02FD388917E34E0DD02072A71748081C02C65(NULL);
+	}
+
+IL_000d:
+	{
+		return;
+	}
+}
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR int32_t ATTrackingStatusBinding_GetAuthorizationTrackingStatus_m84A7C0033255D8E6F420D7F6BC8ED20246A5B577 (const RuntimeMethod* method) 
+{
+	static bool s_Il2CppMethodInitialized;
+	if (!s_Il2CppMethodInitialized)
+	{
+		il2cpp_codegen_initialize_runtime_metadata((uintptr_t*)&Application_tDB03BE91CDF0ACA614A5E0B67CFB77C44EB19B21_il2cpp_TypeInfo_var);
+		s_Il2CppMethodInitialized = true;
+	}
+	{
+		il2cpp_codegen_runtime_class_init_inline(Application_tDB03BE91CDF0ACA614A5E0B67CFB77C44EB19B21_il2cpp_TypeInfo_var);
+		int32_t L_0;
+		L_0 = Application_get_platform_m59EF7D6155D18891B24767F83F388160B1FF2138(NULL);
+		if ((!(((uint32_t)L_0) == ((uint32_t)8))))
+		{
+			goto IL_000e;
+		}
+	}
+	{
+		int32_t L_1;
+		L_1 = ATTrackingStatusBinding_InterfaceGetTrackingAuthorizationStatus_m5BA14B6630FE5730137A92B51E99BEE1AA95DFD7(NULL);
+		return (int32_t)(L_1);
+	}
+
+IL_000e:
+	{
+		return (int32_t)(0);
+	}
+}
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
---
+++ Il2CppOutputProject/Source/il2cppOutput/Unity.Advertisement.IosSupport_CodeGen.c
@@ -0,0 +1,53 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void ATTrackingStatusBinding_InterfaceTrackingAuthorizationRequest_m44D02FD388917E34E0DD02072A71748081C02C65 (void);
+extern void ATTrackingStatusBinding_InterfaceGetTrackingAuthorizationStatus_m5BA14B6630FE5730137A92B51E99BEE1AA95DFD7 (void);
+extern void ATTrackingStatusBinding_RequestAuthorizationTracking_mCBF4621552E7D027AFB9161BA356A0D2E8CC311D (void);
+extern void ATTrackingStatusBinding_GetAuthorizationTrackingStatus_m84A7C0033255D8E6F420D7F6BC8ED20246A5B577 (void);
+static Il2CppMethodPointer s_methodPointers[4] = 
+{
+	ATTrackingStatusBinding_InterfaceTrackingAuthorizationRequest_m44D02FD388917E34E0DD02072A71748081C02C65,
+	ATTrackingStatusBinding_InterfaceGetTrackingAuthorizationStatus_m5BA14B6630FE5730137A92B51E99BEE1AA95DFD7,
+	ATTrackingStatusBinding_RequestAuthorizationTracking_mCBF4621552E7D027AFB9161BA356A0D2E8CC311D,
+	ATTrackingStatusBinding_GetAuthorizationTrackingStatus_m84A7C0033255D8E6F420D7F6BC8ED20246A5B577,
+};
+static const int32_t s_InvokerIndices[4] = 
+{
+	4188,
+	4154,
+	4188,
+	4154,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_Unity_Advertisement_IosSupport_CodeGenModule;
+const Il2CppCodeGenModule g_Unity_Advertisement_IosSupport_CodeGenModule = 
+{
+	"Unity.Advertisement.IosSupport.dll",
+	4,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
